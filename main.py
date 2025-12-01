362e0332ce3034a08a17174876366ee5e8890336
PK_Foobar <kampfhenkeljacob7@gmail.com>
2025-12-01 15:49:10 +0100

    implement book text retrieval and add .gitignore for books directory

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, 'r') as file:
+        text = file.read()
+    return text
+def main():
+    book_text = get_book_text('books/frankenstein.txt')
+    print("Found",num_words(book_text),"total words")
+def num_words(book_text):
+    words = book_text.split()
+    return len(words)
+main()
