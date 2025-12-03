c03531e8f7fed1421924999f26df3b336147488d
PK_Foobar <kampfhenkeljacob7@gmail.com>
2025-12-03 09:07:00 +0100

    refactor main.py to accept book file path as a command-line argument

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words
 from stats import get_num_char
 from stats import sort_char_counts
@@ -8,14 +9,19 @@ def get_book_text(filepath):
     return text
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
     
     char_counts = get_num_char(book_text)
     sorted_chars = sort_char_counts(char_counts)
     
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
